1e1a16877eaf4a04d8d1b57328d09eacabfd051e
Liviu Ionescu <ilg@livius.net>
2018-07-23 00:14:20 +0300

    build.sh: fix target-os macos

--- scripts/build.sh
+++ scripts/build.sh
@@ -264,7 +264,7 @@ else
       host_build_target "Creating the OS X distribution..." \
         --script "${HOST_WORK_FOLDER_PATH}/${CONTAINER_BUILD_SCRIPT_REL_PATH}" \
         --env-file "${ENV_FILE}" \
-        --target-os osx \
+        --target-os macos \
         -- \
         ${rest[@]-}
     else
